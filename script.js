927140cd67b2d0c930c48829c9f821bb4cae1381
mrpsycho-dev <stinkmang6@gmail.com>
2025-01-26 18:35:04 +0500

    FINAL before React rebuild

--- script.js
+++ script.js
@@ -11,49 +11,9 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
-console.log(CATEGORIES.find((cat) => cat.name === "science").color);
-
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-];
-
-const factForm = document.querySelector(".fact-form");
-const shareAFact = document.querySelector(".share");
-const factList = document.querySelector(".fact-list");
-
 factList.innerHTML = "";
-// loadFacts();
-createFactsList(initialFacts);
+loadFacts();
+// createFactsList(initialFacts);
 
 async function loadFacts() {
   const res = await fetch(
@@ -93,6 +53,5 @@ function createFactsList(dataArr) {
     </li>`
   );
   const html = htmlArr.join("");
-  // console.log(htmlArr);
   factList.insertAdjacentHTML("afterbegin", html);
 }
